1596e0c4e803163eea72bdaf56b3fadb75677280
ruslan <don.sabiroff@yandex.ru>
2019-05-30 19:03:11 +0300

    commit

--- hendlers/commonHendlers.js
+++ hendlers/commonHendlers.js
@@ -1,8 +1,10 @@
 var topicsDb = require("../database/topicsdb");
 var usersDb = require("../database/userdb");
 var sessionDb = require("../database/sessiondb");
+
 var isAuthenticated = require("./common").isAuthenticated,
-    isCreaterUser = require("./common").isCreaterUser;
+    isCreaterUser = require("./common").isCreaterUser,
+    isCreaterTopic = require("./common").isCreaterTopic;
 
 function index(req, response) {
     response.sendfile("./index.html");
@@ -17,4 +19,8 @@ exports.isCreaterUser = function(req, res) {
 
 exports.isAuthenticated = function(req, res) {
     isAuthenticated(req, res, () => res.send({ isAuth: true }));
+}
+
+exports.isCreaterTopic = function(req, res) {
+    isCreaterTopic(req, res, () => res.send({ isCreater: true }));
 }
